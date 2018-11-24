3486fc41830bee23d2b2089927ff19e921b8b066
CareXuan <1048474651@qq.com>
2018-11-24 21:36:24 +0800

    vue

---
+++ vueleopard-master/src/views/dashboard/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dashboard-container">
+    hello world
+    {{name}}
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+  import {getInfo} from '../../api/login.js'
+  export default {
+    name: 'dashboard',
+    computed: {
+        name(){
+            return this.$route.path
+        }
+    },
+    mounted:{
+      getInfo().then(function(response){
+        alert(response);
+      })
+    }
+  }
+</script>
+
+<style rel="stylesheet/less" lang="less">
+</style>
